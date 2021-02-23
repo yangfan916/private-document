e6c5b3963f3b55bb6c9b743510b1e55f3c955db4
yangfan <1024337691@qq.com>
2021-02-24 00:29:43 +0800

    面试话术

---
+++ src/main/java/com/yangfan/document/PrivateDocumentApplication.java
@@ -0,0 +1,13 @@
+package com.yangfan.document;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivateDocumentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrivateDocumentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yangfan/document/PrivateDocumentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yangfan.document;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrivateDocumentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
